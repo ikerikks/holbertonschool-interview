f07a12b6c99b5dd8642e3b8d5467ce19dd4a51ba
ikerikks <ikerikks@gmail.com>
2023-08-18 16:21:00 +0200

    lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+
+def loopBox(box, tab, boxes):
+    if len(box) != 0:
+        for key in box:
+            if not tab[key]:
+                tab[key] = True
+                loopBox(boxes[key], tab, boxes)
+
+    return all(tab)
+
+
+def canUnlockAll(boxes):
+    tab = [False] * len(boxes)
+    tab[0] = True
+
+    return loopBox(boxes[0], tab, boxes)
