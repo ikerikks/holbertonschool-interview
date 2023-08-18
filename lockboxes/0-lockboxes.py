2aeb098a5d37d2db074ad873e472ab3664f04e0e
ikerikks <ikerikks@gmail.com>
2023-08-18 17:05:38 +0200

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
-
-'''  function that return a boolean to indicate
-    if all boxes can be unlocked.
-'''
-
-
-def loopBox(box, tab, boxes):
-    if len(box) != 0:
-        for key in box:
-            if not tab[key]:
-                tab[key] = True
-                loopBox(boxes[key], tab, boxes)
-
-    return all(tab)
+""" Python Script
+"""
 
 
 def canUnlockAll(boxes):
-    tab = [False] * len(boxes)
-    tab[0] = True
+    """ Function that determines if all the boxes can be opened
+    Arguments:
+        boxes: list of lists
+    Returns:
+        True if all boxes can be opened, or False otherwise.
+    """
+    unlocked_boxes = set([0])
 
-    return loopBox(boxes[0], tab, boxes)
+    # indices of the boxes not  checked
+    unchecked_boxes = set(range(1, len(boxes)))
+
+    while len(unchecked_boxes) > 0:
+        found_new_key = False
+
+        for box_index in unlocked_boxes.copy():
+            keys = boxes[box_index]
+
+            # try to unlock a new box
+            for key in keys:
+                if key in unchecked_boxes:
+                    unlocked_boxes.add(key)
+                    unchecked_boxes.remove(key)
+                    found_new_key = True
+
+        if not found_new_key:
+            break
+
+    # Check if we have unlocked all the boxes
+    return len(unlocked_boxes) == len(boxes)
 
 
 if __name__ == "__main__":
