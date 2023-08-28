dbec5ce72be1707d061c57939cbb5b8136e213a1
ikerikks <ikerikks@gmail.com>
2023-08-28 14:12:27 +0200

    function update

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -17,13 +17,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
   {
     return NULL;
   }
-
-  if (parent != NULL) {
-    new_node->parent = parent;
-  }
-
+  
   new_node->n = value;
+  new_node->parent = parent;
+  new_node->left = NULL;
+  new_node->right = NULL;
 
-  return new_node;
+  return (new_node);
 
 }
