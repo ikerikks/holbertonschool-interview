5043cbd228afa619abd83b9f557768e046d3637b
ikerikks <ikerikks@gmail.com>
2023-09-20 12:58:54 +0200

    replace of variable

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -13,6 +13,7 @@ int is_palindrome(listint_t **head)
 listint_t *current = *head;
 int order = 0;
 int i = 0;
+int results[order + 1];
 
 while (current->next != NULL)
 {
@@ -20,7 +21,7 @@ order++;
 current = current->next;
 }
 
-int results[order + 1];
+;
 current = *head;
 
 while (current != NULL)
